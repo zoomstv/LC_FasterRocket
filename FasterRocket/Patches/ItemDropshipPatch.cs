46e9ba4b1e4ce4d664136e72afdfa71289c4bcd8
Daniel Hampel <danielhampel.danham@gmail.com>
2023-12-21 12:30:47 +0100

    faster rocket mod

---
+++ FasterRocket/Patches/ItemDropshipPatch.cs
@@ -0,0 +1,35 @@
+﻿using HarmonyLib;
+using Unity.Netcode;
+
+namespace FasterRocket.Patches
+{
+    [HarmonyPatch(typeof(ItemDropship))]
+    internal class ItemDropshipPatch
+    {
+        public static ItemDropshipPatch Instance;
+
+        void Awake()
+        {
+            if (Instance == null)
+            {
+                Instance = this;
+            }
+        }
+
+        [HarmonyPatch("Update")]
+        [HarmonyPostfix]
+        static void UpdateShipTimer(ref float ___shipTimer, ref bool ___deliveringOrder, ItemDropship __instance)
+        {
+            if(((NetworkBehaviour)__instance).IsServer)
+            {
+                if (!___deliveringOrder)
+                {
+                    if (___shipTimer > 2f)
+                    {
+                        ___shipTimer = 50f;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ FasterRocket/Plugin.cs
@@ -0,0 +1,34 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+
+namespace FasterRocket
+{
+    [BepInPlugin(modGUID, modName, modVersion)]
+    public class PluginBase : BaseUnityPlugin
+    {
+        private const string modGUID = "zoomstv.FasterRocket";
+        private const string modName = "FasterRocket";
+        private const string modVersion = "1.0.0.0";
+
+        private readonly Harmony harmony = new Harmony(modGUID);
+
+        private static PluginBase Instance;
+
+        internal static ManualLogSource mls;
+
+        void Awake()
+        {
+            if (Instance == null)
+            {
+                Instance = this;
+            }
+
+            mls = BepInEx.Logging.Logger.CreateLogSource(modGUID);
+
+            mls.LogInfo(modName + " loaded");
+
+            harmony.PatchAll();
+        }
+    }
+}
